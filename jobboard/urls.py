968223e41ad4065359d8d2c2bb54f10cd6ff92c9
Abdul Rahman <abdulnsamba@gmail.com>
2025-08-10 07:42:04 +0300

    fea: added documentation

--- jobboard/urls.py
+++ jobboard/urls.py
@@ -2,9 +2,30 @@ from django.contrib import admin
 from django.urls import path, include
 from jobboard.api_routers import router
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Job Board API",
+        default_version='v1',
+        description="Provides access to the Jobs and applications API",
+        contact=openapi.Contact(email="abdulnsamba@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
+    path('swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
+    path('swagger.json', schema_view.without_ui(
+        cache_timeout=0), name='schema-json'),
 ]
